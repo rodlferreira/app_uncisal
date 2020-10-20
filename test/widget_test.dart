bc482a180cc9be8a87ed9e60e2493930dec31ae9
rodlferreira <58087865+rodlferreira@users.noreply.github.com>
2020-10-20 19:50:57 -0300

    Commit by Marcos F
    The App really started.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,13 +9,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:prototipo_app_uncisal/main.dart';
-// import 'package:teste_arrastar/main.dart';
-
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
