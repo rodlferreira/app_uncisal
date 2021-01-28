644b310698c8a62a7aeb71b1b8c4a0ca7dc4fd6b
alexisbarros <apenasalexis@gmail.com>
2021-01-27 22:39:28 -0300

    update: working in splash screen

--- lib/src/screens/splash/splash_screen.dart
+++ lib/src/screens/splash/splash_screen.dart
@@ -3,23 +3,19 @@ import 'package:flutter/material.dart';
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            'Pygus',
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.black
-            ),
-          ),
-          const SizedBox(height: 38,),
-          CircularProgressIndicator()
-        ],
-      ),
-    );
+    return MaterialApp(
+        home: Scaffold(
+            body: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                      Color.fromRGBO(255, 214, 51, 1.0),
+                      Color.fromRGBO(255, 238, 169, 1.0)
+                    ])),
+                child: Center(
+                    child: Image.asset('assets/images/logo/pygus-logo.png',
+                        height: 200, fit: BoxFit.fill)))));
   }
 }
